fea7bc576e1a9fd21242af3c84de384fce6ba0ba
Franklin <antoniofranklin576@gmail.com>
2024-12-21 09:52:51 -0300

    Refatorando o código

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,29 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import AddTask from "./components/AddTask"
 import Tasks from "./components/Tasks"
 import { v4 } from "uuid"
+import Title from "./components/Title"
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: "Estudar Programação",
-      description: "Estudar programação para se tornar um desenvolvedor full stack",
-      isCompleted: false,
-    },{
-      id: 2,
-      title: "Estudar inglês",
-      description: "Estudar inglês para se tornar fluente",
-      isCompleted: false
-    },
-    {
-      id: 3,
-      title: "Estudar Matemática",
-      description: "Estudar matemática para se tornar um desenvolvedor full stack",
-      isCompleted: false,
-    }
-  ])
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem("tasks")) || []
+  )
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+  }, [tasks])
+
+  // useEffect(() => {
+  //   const fetchTasks = async () => {
+  //     const response = await fetch("https://jsonplaceholder.typicode.com/todos?_limit=10", {
+  //       method: "GET",
+  //     })
+  //     const data = await response.json()
+
+  //     setTasks(data)
+  //   }
+  //   // fetchTasks()
+  // }, [])
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map(task => {
@@ -52,15 +53,16 @@ function App() {
   }
 
   return (
-    <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
-      <div className="w-[500px] space-y-4">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">
-          Gerenciador de Tarefas
-        </h1>
-        <AddTask onAddTaskSubmit={onAddTaskSubmit}/>
-        <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
+    <div className="w-screen h-screen bg-slate-500">
+      <div className="h-auto bg-slate-500 flex justify-center p-6">
+        <div className="w-[500px] space-y-4">
+          <Title>Gerenciador de Tarefas</Title>
+          <AddTask onAddTaskSubmit={onAddTaskSubmit}/>
+          <Tasks tasks={tasks} onTaskClick={onTaskClick} onDeleteTaskClick={onDeleteTaskClick}/>
+        </div>
       </div>
     </div>
+    
   )
 }
 
